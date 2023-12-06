1308c41f894aa11031d4327f84a45646e88bfed9
pedro <pedrofrs008@gmail.com>
2023-12-06 18:05:21 -0300

    Goku

--- miniLocalChat/src/main/java/com/GrupoSD/miniLocalChat/controllers/UsuarioController.java
+++ miniLocalChat/src/main/java/com/GrupoSD/miniLocalChat/controllers/UsuarioController.java
@@ -1,7 +1,5 @@
 package com.GrupoSD.miniLocalChat.controllers;
 
-import java.net.URI;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.GrupoSD.miniLocalChat.models.Usuario;
 import com.GrupoSD.miniLocalChat.models.Usuario.CriarUsuario;
--- miniLocalChat/src/main/java/com/GrupoSD/miniLocalChat/services/UsuarioService.java
+++ miniLocalChat/src/main/java/com/GrupoSD/miniLocalChat/services/UsuarioService.java
@@ -1,6 +1,5 @@
 package com.GrupoSD.miniLocalChat.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
